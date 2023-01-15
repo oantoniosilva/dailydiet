bfcb37e1ade2e0207d2d88b8c7153709ae4499ad
Antonio <antoniopsilva083@gmail.com>
2023-01-15 04:07:51 -0300

    chore: add @expo-fonts and @expo-google-fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import theme from 'src/theme';
+import { ActivityIndicator, Text } from 'react-native';
+
+import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_400Regular,
+    Nunito_700Bold
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      { fontsLoaded ? <Text>Working in Daily Diet app!</Text> : <ActivityIndicator /> }
       <StatusBar style="auto" />
     </ThemeProvider>
   );
